4f8eb7c6ebb404db970d9f675f590662a778cc30
bellujrb <98968823+bellujrb@users.noreply.github.com>
2023-07-08 16:48:41 -0300

    refactory

--- app/lib/ui/views/request_loan/request_loan_screen.dart
+++ app/lib/ui/views/request_loan/request_loan_screen.dart
@@ -3,13 +3,12 @@ import 'package:app/ui/extensions/build_context_utils.dart';
 import 'package:app/ui/styles/colors.dart';
 import 'package:app/ui/styles/text_style.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_modular/flutter_modular.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:toggle_switch/toggle_switch.dart';
 import 'package:percent_indicator/percent_indicator.dart';
 
 import '../../components/header.dart';
 import 'components_this_screen/custom_toggle.dart';
+import 'components_this_screen/request_loan.dart';
 
 class RequestLoanScreen extends StatelessWidget {
   const RequestLoanScreen({Key? key}) : super(key: key);
@@ -36,74 +35,6 @@ class RequestLoanScreen extends StatelessWidget {
   }
 }
 
-class RequestLoan extends StatefulWidget {
-  const RequestLoan({Key? key}) : super(key: key);
-
-  @override
-  State<RequestLoan> createState() => _RequestLoanState();
-}
-
-class _RequestLoanState extends State<RequestLoan> {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: context.mediaWidth * 0.9,
-      height: context.mediaHeight * 0.3,
-      decoration: BoxDecoration(
-          color: AppColors.onPrimary, borderRadius: BorderRadius.circular(8)),
-      child: Column(
-        children: [
-          const SizedBox(
-            height: 15,
-          ),
-          Input(
-              title: "Name Loan",
-              label: "Enter your name loan",
-              type: TextInputType.text,
-              width: context.mediaWidth * 0.8),
-          const CustomToggle(),
-          const SizedBox(
-            height: 15,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                margin: const EdgeInsets.only(top: 10),
-                child: Text(
-                  "R\$",
-                  style: GoogleFonts.poppins(
-                    textStyle: context.appTextStyles.textSmallCommonBlack,
-                  ),
-                ),
-              ),
-              const SizedBox(
-                width: 5,
-              ),
-              Text(
-                "2000",
-                style: GoogleFonts.poppins(
-                    textStyle: context.appTextStyles.textBigBoldBlack),
-              ),
-            ],
-          ),
-          Padding(
-            padding: const EdgeInsets.all(15.0),
-            child: LinearPercentIndicator(
-              width: context.mediaWidth * 0.8,
-              animation: true,
-              lineHeight: 20.0,
-              animationDuration: 2500,
-              percent: 1.0,
-              center: const Text("R\$150 until R\$2000"),
-              progressColor: Colors.green,
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
 
 
 
