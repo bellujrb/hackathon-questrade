2cb3468363aafca54bc75415c0d10bd47070ecfc
bellujrb <98968823+bellujrb@users.noreply.github.com>
2023-07-08 17:24:28 -0300

    all

--- app/lib/ui/styles/text_style.dart
+++ app/lib/ui/styles/text_style.dart
@@ -16,36 +16,37 @@ class AppTextStyles {
       fontSize: 11,
       color: AppColors.onSecondary,
       fontWeight: FontWeight.w500,
-      decoration: TextDecoration.none
-  );
+      decoration: TextDecoration.none);
 
   TextStyle get textMediumBoldBlack => TextStyle(
-    fontSize: 16,
-    color: AppColors.onSecondary,
-    fontWeight: FontWeight.w600,
-    decoration: TextDecoration.none
-  );
+      fontSize: 16,
+      color: AppColors.onSecondary,
+      fontWeight: FontWeight.w600,
+      decoration: TextDecoration.none);
   TextStyle get textBigBoldBlack => TextStyle(
       fontSize: 30,
       color: AppColors.onSecondary,
       fontWeight: FontWeight.w600,
-      decoration: TextDecoration.none
-  );
+      decoration: TextDecoration.none);
 
   //White
   TextStyle get textMediumCommonWhite => TextStyle(
       fontSize: 13,
       color: AppColors.onPrimary,
       fontWeight: FontWeight.w300,
-      decoration: TextDecoration.none
-  );
+      decoration: TextDecoration.none);
   TextStyle get textMediumBoldWhite => TextStyle(
       fontSize: 13,
       color: AppColors.onPrimary,
       fontWeight: FontWeight.w600,
-      decoration: TextDecoration.none
-  );
+      decoration: TextDecoration.none);
 
+  //Gray
+  TextStyle get textMediumCommonGray => TextStyle(
+      fontSize: 12,
+      fontWeight: FontWeight.w400,
+      color: AppColors.tertiary,
+      decoration: TextDecoration.none);
 }
 
 extension AppTextStylesExtension on BuildContext {
--- app/lib/ui/views/request_loan/request_loan_screen.dart
+++ app/lib/ui/views/request_loan/request_loan_screen.dart
@@ -1,13 +1,11 @@
-import 'package:app/ui/components/input.dart';
 import 'package:app/ui/extensions/build_context_utils.dart';
 import 'package:app/ui/styles/colors.dart';
-import 'package:app/ui/styles/text_style.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:percent_indicator/percent_indicator.dart';
+import 'package:roundcheckbox/roundcheckbox.dart';
 
 import '../../components/header.dart';
-import 'components_this_screen/custom_toggle.dart';
+import '../../components/status_score.dart';
 import 'components_this_screen/request_loan.dart';
 
 class RequestLoanScreen extends StatelessWidget {
@@ -20,14 +18,24 @@ class RequestLoanScreen extends StatelessWidget {
       body: MaterialApp(
         debugShowCheckedModeBanner: false,
         home: SafeArea(
-          child: Column(
-            children: [
-              const Header(),
-              SizedBox(
-                height: context.mediaHeight * 0.03,
-              ),
-              const RequestLoan()
-            ],
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                const Header(),
+                SizedBox(
+                  height: context.mediaHeight * 0.03,
+                ),
+                const RequestLoan(),
+                SizedBox(
+                  height: context.mediaHeight * 0.03,
+                ),
+                const TermContainer(),
+                SizedBox(
+                  height: context.mediaHeight * 0.03,
+                ),
+                const StatusScore()
+              ],
+            ),
           ),
         ),
       ),
@@ -35,7 +43,112 @@ class RequestLoanScreen extends StatelessWidget {
   }
 }
 
+class TermContainer extends StatelessWidget {
+  const TermContainer({Key? key}) : super(key: key);
 
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: context.mediaHeight * 0.4,
+      width: context.mediaWidth * 0.9,
+      decoration: BoxDecoration(
+          color: AppColors.onPrimary, borderRadius: BorderRadius.circular(8)),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Container(
+            width: context.mediaWidth * 0.8,
+            height: context.mediaHeight * 0.15,
+            decoration: BoxDecoration(
+                color: AppColors.primary,
+                borderRadius: BorderRadius.circular(16)),
+            child: const ButtonRead(),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                RoundCheckBox(
+                  onTap: (selected) {},
+                ),
+                SizedBox(
+                  width: context.mediaWidth * 0.6,
+                  child: Text(
+                    "I agree with the term of responsibility when applying for the loan with QUESTRADE.",
+                    style: GoogleFonts.poppins(
+                        textStyle: context.styleModifier.textMediumCommonGray),
+                  ),
+                )
+              ],
+            ),
+          ),
+          GestureDetector(
+            child: Container(
+              alignment: Alignment.center,
+              width: context.mediaWidth * 0.8,
+              height: 40,
+              decoration: BoxDecoration(
+                  color: AppColors.primary,
+                  borderRadius: BorderRadius.circular(16)),
+              child: Text(
+                "Request",
+                style: GoogleFonts.poppins(
+                    textStyle: context.styleModifier.textMediumBoldWhite),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
 
+class ButtonRead extends StatelessWidget {
+  const ButtonRead({Key? key}) : super(key: key);
 
-
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(left: 5),
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Text(
+                "Statement of Responsibility",
+                style: GoogleFonts.poppins(
+                    textStyle: context.styleModifier.textMediumCommonWhite),
+              ),
+              GestureDetector(
+                // Logic
+                child: Container(
+                  alignment: Alignment.center,
+                  width: context.mediaWidth * 0.3,
+                  height: 30,
+                  decoration: BoxDecoration(
+                      color: AppColors.onPrimary,
+                      borderRadius: BorderRadius.circular(16)),
+                  child: Text(
+                    "Read",
+                    style: GoogleFonts.poppins(
+                        textStyle: context.styleModifier.textMediumBoldBlack),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            width: 60.34,
+            height: 68,
+            child: Image.asset('assets/term.png'),
+          ),
+        ],
+      ),
+    );
+  }
+}
